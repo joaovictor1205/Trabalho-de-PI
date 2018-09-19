b4e26b7b43494c2224d1551e26a589fb998a2597
joaovictor1205 <joaovictor.lol12@gmail.com>
2018-09-19 00:29:44 -0300

    Continuação

---
+++ home.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    
+    $("#SejaBemVindo").click(function(){
+        $("#slide").slideToggle(500);
+    });
+
+    $("#Missao").click(function(){
+        $("#slide2").slideToggle(500);
+    });
+
+    $("#Valores").click(function(){
+        $("#slide3").slideToggle(500);
+    });
+
+});
