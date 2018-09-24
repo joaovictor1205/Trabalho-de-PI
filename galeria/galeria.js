f20a08bcff147e8342bf73847df26331dd9960c4
joaovictor1205 <joaovictor.lol12@gmail.com>
2018-09-24 10:45:08 -0300

    Continuação Tela Galeria

--- galeria/galeria.js
+++ galeria/galeria.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
-    $('.imggaleria').hide().delay().fadeIn(2500);  
+    $('.imggaleria').hide().delay().fadeIn(3000); 
 
+    
 });
+
+function addBorder(e){
+    e.classList.add('border-red');
+}
+
+function removeBorder(e){
+    e.classList.remove('border-red');
+}
