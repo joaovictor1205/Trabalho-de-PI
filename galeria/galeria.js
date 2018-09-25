b738af7c35657a963203bc09ab7ef83d764b22d7
joaovictor1205 <joaovictor.lol12@gmail.com>
2018-09-25 13:45:04 -0300

    Footer da Tela Home

--- galeria/galeria.js
+++ galeria/galeria.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
 
     $('.imggaleria').hide().delay().fadeIn(3000); 
-
     
 });
 
 function addBorder(e){
+
     e.classList.add('border-red');
+
 }
 
 function removeBorder(e){
+
     e.classList.remove('border-red');
+    
 }
