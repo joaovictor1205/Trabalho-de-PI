93e5a47f6692694895785c057e2617e86afd9aa5
Diogo Canut <canut@wcore.com.br>
2018-11-07 00:11:53 -0200

    login

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php 
+// session_start inicia a sessão
+session_start();
+
+$login = $_POST['login'];
+$senha = $_POST['senha'];
+
+require "conexaoBanco.php";
+
+
+  
+$conn = conectaAoMySQL();
+$select = mysqli_select_db("server") or die("Sem acesso ao DB, Entre em 
+contato com o Administrador, gilson_sales@bytecode.com.br");
+ 
+
+$result = mysqli_query("SELECT * FROM `USUARIO` 
+WHERE `NOME` = '$login' AND `SENHA`= '$senha'");
+
+if(mysqli_num_rows ($result) > 0 )
+{
+$_SESSION['login'] = $login;
+$_SESSION['senha'] = $senha;
+header('location:homePrivado.html');
+}
+else{
+  unset ($_SESSION['login']);
+  unset ($_SESSION['senha']);
+  header('location:home.html');
+   
+  }
+?>
